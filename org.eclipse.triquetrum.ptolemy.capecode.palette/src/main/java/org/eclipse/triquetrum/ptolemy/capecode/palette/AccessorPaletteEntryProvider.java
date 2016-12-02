b891673793ea087ba8389ae1accad84e8dc17416
Erwin De Ley <erwindl0@gmail.com>
2016-12-02 10:14:11 +0100

    trials for loading an AccessLibrary with 1 accessor
    from a local index.json and accessor js file
    
    Signed-off-by: Erwin De Ley <erwindl0@gmail.com>

--- org.eclipse.triquetrum.ptolemy.capecode.palette/src/main/java/org/eclipse/triquetrum/ptolemy/capecode/palette/AccessorPaletteEntryProvider.java
+++ org.eclipse.triquetrum.ptolemy.capecode.palette/src/main/java/org/eclipse/triquetrum/ptolemy/capecode/palette/AccessorPaletteEntryProvider.java
@@ -1,24 +1,26 @@
 package org.eclipse.triquetrum.ptolemy.capecode.palette;
 
+import static org.eclipse.triquetrum.workflow.editor.palette.spi.PaletteConfigurationElement.CLASS;
+import static org.eclipse.triquetrum.workflow.editor.palette.spi.PaletteConfigurationElement.DISPLAY_NAME;
+import static org.eclipse.triquetrum.workflow.editor.palette.spi.PaletteConfigurationElement.ICON;
+import static org.eclipse.triquetrum.workflow.editor.palette.spi.PaletteConfigurationElement.TYPE;
+
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.BufferUnderflowException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.triquetrum.workflow.editor.palette.spi.PaletteConfigurationElement;
 import org.eclipse.triquetrum.workflow.editor.palette.spi.PaletteEntryProvider;
-import org.osgi.framework.Bundle;
 import org.terraswarm.accessor.AccessorLibrary;
 
-import ptolemy.actor.lib.CurrentTime;
 import ptolemy.kernel.CompositeEntity;
+import ptolemy.kernel.Entity;
 import ptolemy.kernel.util.IllegalActionException;
 import ptolemy.kernel.util.NameDuplicationException;
-
-import static org.eclipse.triquetrum.workflow.editor.palette.spi.PaletteConfigurationElement.*;
 
 public class AccessorPaletteEntryProvider implements PaletteEntryProvider {
 
@@ -26,18 +28,22 @@ public class AccessorPaletteEntryProvider implements PaletteEntryProvider {
   public IConfigurationElement[] getPaletteEntries() {
     try {
       AccessorLibrary accessorLibrary = buildAccessorLibrary();
+      List<IConfigurationElement> results = new ArrayList<>();
+      for (Entity<?> accessor : (List<Entity<?>>) accessorLibrary.entityList()) {
+        Map<String, String> attributes = new HashMap<>();
+        attributes.put(CLASS, accessor.getClass().getName());
+        attributes.put(DISPLAY_NAME, accessor.getDisplayName());
+        attributes.put(ICON, "icons/time_obj.gif");
+        attributes.put(TYPE, "Actor");
+        PaletteConfigurationElement pce = new PaletteConfigurationElement("entry", "org.eclipse.triquetrum.ptolemy.capecode.palette", attributes );
+        results.add(pce);
+      }
+      return results.toArray(new IConfigurationElement[0]);
     } catch (IllegalActionException | NameDuplicationException | MalformedURLException e) {
       // TODO Auto-generated catch block
       e.printStackTrace();
+      return new IConfigurationElement[] {};
     }
-    System.err.println("I was here!");
-    Map<String, String> attributes = new HashMap<>();
-    attributes.put(CLASS, CurrentTime.class.getName());
-    attributes.put(DISPLAY_NAME, "Current Time");
-    attributes.put(ICON, "icons/time_obj.gif");
-    attributes.put(TYPE, "Actor");
-    PaletteConfigurationElement pce = new PaletteConfigurationElement("entry", "org.eclipse.triquetrum.ptolemy.capecode.palette", attributes );
-    return new IConfigurationElement[] {pce};
   }
 
   private AccessorLibrary buildAccessorLibrary() throws IllegalActionException, NameDuplicationException, MalformedURLException {
